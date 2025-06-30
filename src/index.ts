db1d5ddf8ed28ab2928efa42e120910631c091b5
Brandon Payton <brandon@happycode.net>
2025-06-30 00:36:59 -0400

    Sketch more of the messaging implementation

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,81 @@
 import type { Worker as NodeWorker } from 'worker_threads';
 
-const resultCache = new WeakMap<SharedArrayBuffer, any>();
-
 interface Endpoint {
-	addEventListener: (type: string, listener: (event: any) => void) => void;
-	removeEventListener: (type: string, listener: (event: any) => void) => void;
-	postMessage: (message: any) => void;
+	on: NodeWorker['on'],
+	off: NodeWorker['off'],
+	postMessage: NodeWorker['postMessage']
 }
+
+type ReleaseFunction = () => void;
+
+type NonEmptyArray<T> = [T, ...T[]];
+type RemoteAction_Get = {
+	type: 'get',
+	path: NonEmptyArray<string>,
+	responseBuffer: SharedArrayBuffer,
+}
+type RemoteAction_Set = {
+	type: 'set',
+	path: NonEmptyArray<string>,
+	value: any,
+	responseBuffer: SharedArrayBuffer,
+}
+type RemoteAction_Apply = {
+	type: 'apply',
+	path: NonEmptyArray<string>,
+	args: any[],
+	responseBuffer: SharedArrayBuffer,
+}
+type RemoteAction = RemoteAction_Get | RemoteAction_Set | RemoteAction_Apply;
+
+// @TODO list supported response types
+
+const resultCache = new WeakMap<SharedArrayBuffer, any>();
+const symbolForRelease = Symbol('release');
 
 export function expose<T>(
+	name: string,
 	value: T,
 	// @TODO: Make this a generic type so we can accommodate other worker types.
-	endpoint: NodeWorker
-): T {
-	return value;
+	endpoint: Endpoint
+): ReleaseFunction {
+	function onMessage(event: MessageEvent) {
+		// @TODO: Warn if event doesn't have expected properties.
+
+		const request = event.data;
+		
+
+		switch (request.type) {
+
+			
+		}
+	}
+
+	endpoint.on('message', onMessage);
+	return () => {
+		endpoint.off('message', onMessage);
+	}	
 }
 
-export function wrap<T>() {
+type Wrapped<T> = T & {
+	[symbolForRelease]: ReleaseFunction,
+}
+
+export function wrap<T>(
+	name: string,
+): Wrapped<T> {
 	// @TODO: Support releasing the proxy.
 	return new Proxy<T>({} as T, {
 		// TODO: get
 		// TODO: set
 		// TODO: apply
-	});
+	}) as Wrapped<T>;
+}
+
+export function releaseWrapped<T>(
+	wrapped: Wrapped<T>
+): void {
+	if (symbolForRelease in wrapped) {
+		wrapped[symbolForRelease]();
+	}
 }
